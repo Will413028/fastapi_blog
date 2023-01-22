ea3356b5d0fedfb9997dce4d34d93fdf34973954
Will <will413028@gmail.com>
2023-01-22 14:36:10 +0800

    feat: add description to comment api

--- main.py
+++ main.py
@@ -24,13 +24,22 @@ def hello():
     return {'message': 'Hello world'}
 
 
-@app.get('/blog/all', tags=['blog'])
+@app.get('/blog/all', tags=['blog'], summary="Retrieve all blogs", description="This api call simuates fetching all blogs")
 def get_all_blog(page = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
 
 @app.get('/blog/{id}/comments/{comment_id}', tags=['comments'])
 def get_comment(id:int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **vaild** optional query parameter
+    - **username** optional query parameter
+
+    """
     return {'message': f'blog_id {id},  comment_id {comment_id}, valid {valid}, username {username}   '}
 
 
