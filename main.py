7257988c228a72d9b2e57ff8b74aaa10cb23fa1e
Will <will413028@gmail.com>
2023-01-27 22:16:03 +0800

    feat: add article apis and relationship between user and articles

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from router import blog_api
 from router import user_api
+from router import article_api
 from db.database import engine
 from db import models
 
 app = FastAPI()
 app.include_router(user_api.router)
 app.include_router(blog_api.router)
+app.include_router(article_api.router)
 
 @app.get('/hello')
 def hello():
--- schemas.py
+++ schemas.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
+class Article(BaseModel):
+  title: str
+  content: str
+  published: bool
+  class Config():
+    orm_mode = True
+
 class UserBase(BaseModel):
   username: str
   email: str
@@ -8,5 +15,27 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
   username: str
   email: str
+  items: list[Article] = []
+  class Config():
+    orm_mode = True
+
+class User(BaseModel):
+  id: int
+  username: str
+  class Config():
+    orm_mode = True
+
+
+class ArticleBase(BaseModel):
+  title: str
+  content: str
+  published: bool
+  creator_id: int
+
+class ArticleDisplay(BaseModel):
+  title: str
+  content: str
+  published: bool
+  user: User
   class Config():
     orm_mode = True
