355ff9250e11b101b9e776ae2912f171cfb6b6df
Will <will413028@gmail.com>
2023-01-28 22:05:59 +0800

    feat: add download file api

--- router/file_api.py
+++ router/file_api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, File, UploadFile
+from fastapi.responses import FileResponse
 import shutil
 
 router = APIRouter(
@@ -23,3 +24,10 @@ def get_upload_file(upload_file: UploadFile = File(...)):
         'filename': path,
         'type': upload_file.content_type
     }
+
+
+
+@router.get('/download/{file}', response_class=FileResponse)
+def get_file(name: str):
+    path = f"files/{name}"
+    return path
