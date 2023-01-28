09693ae6a518056e40182e19c854bde674c7ae19
Will <will413028@gmail.com>
2023-01-28 12:50:18 +0800

    feat: cors middleware

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from router import user_api
 from router import article_api
 from db.database import engine
 from db import models
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user_api.router)
@@ -15,3 +16,15 @@ def hello():
     return {'message': 'Hello world'}
 
 models.Base.metadata.create_all(engine)
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
