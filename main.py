a3c490770e57bd64e8ed732b04db5a371db13f60
Will <will413028@gmail.com>
2023-01-29 01:11:44 +0800

    feat: add middleware

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from router import blog_api, user_api, article_api, file_api
 from auth import authentication
 from db.database import engine
 from db import models
 from fastapi.middleware.cors import CORSMiddleware
+import time
 
 app = FastAPI()
 app.include_router(authentication.router)
@@ -17,6 +18,15 @@ def hello():
     return {'message': 'Hello world'}
 
 models.Base.metadata.create_all(engine)
+
+@app.middleware("http")
+async def add_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    response.headers['duration'] = str(duration)
+    return response 
+
 
 origins = [
     'http://localhost:3000'
